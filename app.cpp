bf7c67fff11d2d1f3f78bdaa095cc13bd284f1d1
MerkulovMarsel <144001011+MerkulovMarsel@users.noreply.github.com>
2024-08-05 23:27:26 +0300

    start upd

---
+++ app.cpp
@@ -0,0 +1,37 @@
+#include <sudoku/sudoku.hpp>
+
+
+class SudokuApp {
+public:
+  SudokuApp() = default;
+  void Initialaze(int argc, char** argv);
+
+
+  void Update_App();
+
+
+
+
+
+private:
+  
+};
+
+class SudokuObject {
+public:
+  SudokuObject();
+
+
+
+
+private:
+  const SudokuApp* m_app;
+};
+
+
+
+
+
+int main(int argc, char** argv ) {
+
+}
